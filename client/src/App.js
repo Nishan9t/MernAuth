7ccf18c368b5792510827be474df1a46c6da10ea
Nishan9t <pandeynisha9t@gmail.com>
2023-08-18 13:06:35 +0530

    assign routes is complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
-
+import {Route,Routes,Navigate} from 'react-router-dom';
+import Main from './components/Main';
+import Signup from './components/signup/index';
+import Login from './components/login/index';
 
 
 function App() {
+  const user=localStorage.getItem("token")
   return (
-    <div className="App">
-     hello world
-    </div>
+    <Routes>
+      {user && <Route path='/' exact element={<Main/>} />}
+      <Route path='/signup' exact element={<Signup/>} />
+      <Route path='/login' exact element={<Login/>} />
+      <Route path='/' exact element={<Navigate replace to="/login" />} />
+
+    </Routes>
   );
 }
 
